1601bbb74e101b18ac900a613cb10d0e664d8ef5
Isuzu <ballage94@gmail.com>
2023-04-18 09:39:19 +0300

    adding comment

--- EKZ/TeamRocket/scripts/controller/taskPage.js
+++ EKZ/TeamRocket/scripts/controller/taskPage.js
@@ -7,16 +7,42 @@ export function clickableTasks() {
     const allTasks = document.querySelectorAll('.taskForm');
     allTasks.forEach(e => e.addEventListener('click', () => {
         if (currentTaskOpen) currentTaskOpen.remove();
-
-        console.log(e.id);
-        taskPage.showTaskPage(mainDB.getTask(e.id));
-
-        const closeButton = document.querySelector('.task .crossIco');
-        currentTaskOpen = document.querySelector('.task');
-
-        closeButton.addEventListener('click', () => {
-            currentTaskOpen.remove();
-            currentTaskOpen = null;
-        });
+        const task = mainDB.getTask(e.id);
+        taskPageFunctional(task)
     }))
+}
+
+function writeComment(task) {
+    const commentText = document.querySelector('textarea[name="newComment"]')
+    const commentWordsCounter = document.querySelector('.newComment span')
+    commentText.addEventListener('input', (event) => {
+        let count = commentText.value.length;
+        if (count >= 280) {
+            commentText.value = commentText.value.slice(0, 280)
+            count = 280
+        }
+        commentWordsCounter.innerHTML = `${count}/280`
+    })
+    const sendButton = document.querySelector('.postComment')
+    sendButton.addEventListener('click', () => {
+        task.addComment(commentText.value)
+
+        taskPageFunctional(task)
+    })
+}
+
+function taskPageFunctional(task) {
+    if (currentTaskOpen) currentTaskOpen.remove();
+
+    taskPage.showTaskPage(task);
+
+    writeComment(task)
+
+    const closeButton = document.querySelector('.task .crossIco');
+    currentTaskOpen = document.querySelector('.task');
+
+    closeButton.addEventListener('click', () => {
+        currentTaskOpen.remove();
+        currentTaskOpen = null;
+    });
 }
