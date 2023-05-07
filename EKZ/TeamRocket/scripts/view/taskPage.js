981c1a2878721fc70ded0bd92cdcba8ef2710bb7
RiverBeaver <maria.a.badanina@gmail.com>
2023-05-07 14:38:11 +0300

    fix date comments

--- EKZ/TeamRocket/scripts/view/taskPage.js
+++ EKZ/TeamRocket/scripts/view/taskPage.js
@@ -20,13 +20,16 @@ export class TaskPage {
 
     returnPageHTML(task) {
         const pageHTML = view.get('taskPage');
+
         pageHTML.querySelector('.assignTo select').value = task.assignee;
         pageHTML.querySelector('.title input').value = task.name;
         pageHTML.querySelector('.description textarea').value = task.description;
         pageHTML.querySelector('.boardName').innerText = task.status;
-        this.returnComments(task).forEach(comment => {
-            pageHTML.querySelector('.commentSection').append(comment);
-        })
+        
+        const comments = this.returnComments(task);
+        for (let i = comments.length - 1; i >= 0; i--) {
+            pageHTML.querySelector('.commentSection').append(comments[i]);
+        }
 
         return pageHTML;
     }
@@ -38,14 +41,52 @@ export class TaskPage {
     returnCommentHTML(comment) {
         const commentHTML = view.get('comment');
         commentHTML.querySelector('p').innerText = comment.text;
-        commentHTML.querySelector('.date').innerText = comment.createdAt;
+        commentHTML.querySelector('.date').innerText = this._getDateCreatComment(comment);
         
         return commentHTML;
     }
 
     _getDateCreatComment(comment) {
-        const date = Date.parse(comment.createdAt);
-        
+        const date = new Date(Date.parse(comment.createdAt));
+        const currentDate = new Date();
+        const difference = Math.floor((currentDate - date) / 1000);
+        console.log(difference);
+
+        if ((Math.floor(difference / 60)) === 0) {
+            return `${difference} seconds ago`;
+
+        } else if (Math.floor(difference / (60 * 60)) === 0) {
+            return `${Math.floor(difference / 60)} minutes ago`;
+
+        } else if (Math.floor(difference / (60 * 60 * 24)) === 0) {
+            return `${Math.floor(difference / (60 * 24))} hours ago`;
+
+        } else {
+            const year = date.getFullYear();
+            const month = this._getMonth(date.getMonth());
+            const day = date.getDate();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+
+             return `${day} ${month}, ${year} at ${hours}:${minutes}`;
+        }
+    }
+
+    _getMonth(month) {
+        switch (month) {
+            case 0: return 'January';
+            case 1: return 'February';
+            case 2: return 'March';
+            case 3: return 'April';
+            case 4: return 'May';
+            case 5: return 'June';
+            case 6: return 'July';
+            case 7: return 'August';
+            case 8: return 'September';
+            case 9: return 'October';
+            case 10: return 'November';
+            case 11: return 'December';
+        }
     }
 
     setCurrentImportance(task) {
