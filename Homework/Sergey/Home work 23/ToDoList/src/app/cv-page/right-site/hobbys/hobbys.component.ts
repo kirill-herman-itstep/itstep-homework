531366dbfb5f9256690030945017a3f20a9039a2
Sergey Masalski <masalskiysergey@gmail.com>
2023-06-02 16:17:06 +0300

    HW/Sergey_Masalski/Angular_CV

---
+++ Homework/Sergey/Home work 23/ToDoList/src/app/cv-page/right-site/hobbys/hobbys.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobbys',
+  templateUrl: './hobbys.component.html',
+  styleUrls: ['./hobbys.component.scss'],
+})
+export class HobbysComponent {}
